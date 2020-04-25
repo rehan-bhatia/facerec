babde95b3f7e1893812ae240475934b869994a65
rehan-bhatia <63210052+rehan-bhatia@users.noreply.github.com>
2020-04-26 01:18:56 +0530

    Updates

---
+++ precache-manifest.1f4cb0e03e3ed910e9fc410339f8b0a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90326ea2d64cca64a4edf6289798f42",
+    "url": "/facerec/index.html"
+  },
+  {
+    "revision": "89badfde9fb4595355c7",
+    "url": "/facerec/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "3ebce1eebd17887ac120",
+    "url": "/facerec/static/css/main.a0a65f3c.chunk.css"
+  },
+  {
+    "revision": "89badfde9fb4595355c7",
+    "url": "/facerec/static/js/2.7945b432.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerec/static/js/2.7945b432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ebce1eebd17887ac120",
+    "url": "/facerec/static/js/main.db329604.chunk.js"
+  },
+  {
+    "revision": "449b890d7bc6cd299b31",
+    "url": "/facerec/static/js/runtime-main.d0daa927.js"
+  }
+]);
